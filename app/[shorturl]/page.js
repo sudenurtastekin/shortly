ea651dbdc73d5024e84bd72a6e0a57aed9f1c009
Sudenur Taştekin <sude348tastekin@gmail.com>
2024-10-02 19:28:12 +0300

    first commit

---
+++ app/[shorturl]/page.js
@@ -0,0 +1,25 @@
+import { redirect } from 'next/navigation';
+
+const ShortUrlPage = async ({ params }) => {
+  const { shorturl } = params;
+
+  const response = await fetch(`https://nkmvhnwulrqvvmnzfmdz.supabase.co/rest/v1/urls?short_url=eq.${shorturl}`, {
+    headers: {
+      "apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5rbXZobnd1bHJxdnZtbnpmbWR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc4MDI2NjAsImV4cCI6MjA0MzM3ODY2MH0.57lDrq8eEdkSO_CpN-rSacX6j_cljo2_RA_tlp00Jl0",
+      "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5rbXZobnd1bHJxdnZtbnpmbWR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc4MDI2NjAsImV4cCI6MjA0MzM3ODY2MH0.57lDrq8eEdkSO_CpN-rSacX6j_cljo2_RA_tlp00Jl0"
+    }
+  });
+
+  const data = await response.json();
+
+  if (data.length > 0) {
+    const longUrl = data[0].long_url; 
+
+    redirect(longUrl);
+  } else {
+   
+    redirect('/404');
+  }
+};
+
+export default ShortUrlPage;
---
+++ app/shortly/components/LinkArea.js
@@ -0,0 +1,71 @@
+"use client";
+import { useState } from 'react';
+
+const LinkArea = () => {
+  const [longUrl, setLongUrl] = useState('');
+  const [shortUrl, setShortUrl] = useState('');
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(null);
+
+    try {
+      const generatedShortUrl = generateShortUrl(); 
+
+      const response = await fetch("https://nkmvhnwulrqvvmnzfmdz.supabase.co/rest/v1/urls", {
+        method: "POST",
+        headers: {
+          "apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5rbXZobnd1bHJxdnZtbnpmbWR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc4MDI2NjAsImV4cCI6MjA0MzM3ODY2MH0.57lDrq8eEdkSO_CpN-rSacX6j_cljo2_RA_tlp00Jl0",
+          "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5rbXZobnd1bHJxdnZtbnpmbWR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc4MDI2NjAsImV4cCI6MjA0MzM3ODY2MH0.57lDrq8eEdkSO_CpN-rSacX6j_cljo2_RA_tlp00Jl0",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          long_url: longUrl,
+          short_url: generatedShortUrl
+        })
+      });
+
+      if (response.ok) {
+        setShortUrl(generatedShortUrl); 
+      } else {
+        throw new Error('Short URL oluşturulamadı');
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  const generateShortUrl = () => {
+    return Math.random().toString(36).substring(2, 8); 
+  };
+
+  return (
+    <div className="linkArea">
+      <form id="link-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="link-input"
+          placeholder="Shorten a link here..."
+          value={longUrl}
+          onChange={(e) => setLongUrl(e.target.value)} 
+          required
+        />
+        <button className='shortenBtn' type="submit">Shorten It!</button>
+      </form>
+
+      {shortUrl && (
+        <div id="shortened-link">
+          <p className='longurl'>{longUrl}</p>
+          <a id="shortened-url" href={`/${shortUrl}`} target="_blank">
+            {`${window.location.origin}/${shortUrl}`}
+          </a>
+        </div>
+      )}
+
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+    </div>
+  );
+};
+
+export default LinkArea;
